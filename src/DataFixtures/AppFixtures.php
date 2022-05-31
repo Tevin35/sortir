d25053a3509840ce630d1eb0314df717fed91d64
Florian <florian.lebodic2022@campus-eni.fr>
2022-05-31 16:26:49 +0200

    Add of one participant link with one campus and hash password, now just need to loop in for a big data base of ten or twenty participants :D

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -13,9 +14,11 @@ class AppFixtures extends Fixture
 
     private $encoder;
     private $faker;
-    private $manager;
+    private ObjectManager $manager;
 
-    public function __construct(UserPasswordHasherInterface $encoder){
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
         $this->encoder = $encoder;
         $this->faker = Factory::create('fr_FR');
     }
@@ -23,21 +26,62 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        $this->manager = $manager;
 
-        $participant = new Participant();
+        $this->addCampus($manager);
+        $this->addParticipant($manager);
 
-        $participant
-            ->setPseudo($faker->userName())
-            ->setLastName($faker->lastName())
-            ->setLastName($faker->lastName())
-            ->setPhone($faker->phoneNumber())
-            ->setEmail('user@test.com')
-            ->
+    }
 
+    private function addCampus(ObjectManager $manager)
+    {
+        $campus1 = new Campus();
+        $campus1->setName('Niort');
+        $manager->persist($campus1);
 
+        $campus2 = new Campus();
+        $campus2->setName('Rennes');
+        $manager->persist($campus2);
 
+        $campus3 = new Campus();
+        $campus3->setName('Nantes');
+        $manager->persist($campus3);
 
+        $campus4 = new Campus();
+        $campus4->setName('Quimper');
+        $manager->persist($campus4);
 
         $manager->flush();
     }
+
+    private function addParticipant(ObjectManager $manager)
+    {
+        $participant = new Participant();
+
+        $campus = $this->manager->getRepository(Campus::class)->FindAll();
+        $participant
+            ->setPseudo($this->faker->userName())
+            ->setLastName($this->faker->lastName())
+            ->setFirstName($this->faker->firstName())
+            ->setPhone($this->faker->phoneNumber())
+            ->setEmail($this->faker->email)
+            ->setRoles(['ROLE_USER'])
+            ->setCampus($this->faker->randomElement($campus))
+            ->setActive(true);
+        $password = $this->encoder->hashPassword($participant, 'password');
+        $participant->setPassword($password);
+        $manager->persist($participant);
+        $manager->flush();
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
