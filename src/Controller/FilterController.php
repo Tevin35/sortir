b975ba679c6aebe2990d213a58aaa8ea50d6c93f
Florian <florian.lebodic2022@campus-eni.fr>
2022-06-04 12:55:13 +0200

    Register action working !

--- src/Controller/FilterController.php
+++ src/Controller/FilterController.php
@@ -9,18 +9,26 @@ use App\Entity\Trip;
 use App\Form\FilterType;
 use App\Form\Model\SearchData;
 use App\Repository\TripRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+
 class FilterController extends AbstractController
 {
+
+
+
+
     #[Route('/filter', name: 'filter')]
     public function index(TripRepository $tripRepository, Request $request): Response
     {
 
-    //Pas besoin de getUser ici car on le place dans le constructeur du repository comme dans les fixtures au final
+        //Pas besoin de getUser ici car on le place dans le constructeur du repository comme dans les fixtures au final
         //On sera en revanche obligé de déclarer une variable avant.
 
         $trip = new Trip();
@@ -29,23 +37,15 @@ class FilterController extends AbstractController
         $SearchData = new SearchData();
         $state = new State();
 
-
-
-
-
         //Create form who use data
         $form = $this->createForm(FilterType::class, $SearchData);
         $form->handleRequest($request);
         $listTrips = $tripRepository->findSearch($SearchData, $state);
 
-
-
-        if($form -> isSubmitted()) {
+        if ($form->isSubmitted()) {
             $listTrips = $tripRepository->findSearch($SearchData, $state);
             dump($listTrips);
         }
-
-
 
 
         return $this->render('filter/index.html.twig', [
@@ -53,4 +53,44 @@ class FilterController extends AbstractController
             'filterForm' => $form->createView()
         ]);
     }
+
+
+    #[Route('/register/{id}', name: 'register')]
+    public function register($id, TripRepository $tripRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        /**
+         * pour avoir l'autocomplétion de tous les attributs de Participant
+         * @var Participant $currentUser
+         */
+
+        $currentUser = $this->getUser();
+//        dd($currentUser);
+        $trip = $tripRepository->find($id);
+        $trip->addRegisteredParticipant($currentUser);
+        $em->persist($trip);
+        $em->flush();
+
+        return $this->redirectToRoute('filter');
+
+
+    }
+
+
+
+
+
+    #[Route('/unsubscribe', name: 'unsubscribe')]
+    public function unsubscribe(TripRepository $tripRepository, Request $request): Response
+    {
+        /**
+         * pour avoir l'autocomplétion de tous les attributs de Participant
+         * @var Participant $currentUser
+         */
+        $trip = new Trip();
+        $currentUser = $this->getUser();
+        $trip->removeRegisteredParticipant($currentUser);
+        return $this->redirectToRoute('filter');
+
+    }
+
 }
